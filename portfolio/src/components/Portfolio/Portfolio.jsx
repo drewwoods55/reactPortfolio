cc8d7e79cc9b76ee042f43d8bf6c32a28e5370b6
Andrew  Woods <andrewwoods@Andrews-MacBook-Pro.local>
2022-04-18 22:49:39 -0400

    completed nav bar and menu button hamburger button

---
+++ portfolio/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Portfolio.scss"
+
+function Portfolio() {
+    return (
+        <div className="Portfolio" id="portfolio">
+            
+        </div>
+    )
+}
+
+export default Portfolio
---
+++ portfolio/src/components/Topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./topbar.scss"
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+function Topbar({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">
+                        portfolio.
+                    </a>
+                    <div className="itemContainer">
+                        <PersonIcon className="icon" />
+                        <span>+44 924 12 74</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailIcon className="icon" />
+                        <span>drewwoods9@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Topbar
